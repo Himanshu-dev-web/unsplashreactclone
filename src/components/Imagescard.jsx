788c3e543336ecbdfd95904af9013c378c91d536
Himanshu-dev-web <hims213174@gmail.com>
2023-10-19 23:20:24 +0530

    did some changes

--- src/components/Imagescard.jsx
+++ src/components/Imagescard.jsx
@@ -4,13 +4,13 @@ import Singlecard from "./Singlecard";
 const Imagescard = ({ images }) => {
   const [loading, setLoading] = useState(true);
   
-  useEffect(() => {
-    if (images) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-    }
-  }, [images]);
+  // useEffect(() => {
+  //   if (images) {
+  //     setLoading(false);
+  //   } else {
+  //     setLoading(true);
+  //   }
+  // }, [images]);
   return (
     <>
       <div className="    gap-0  columns-1 md:columns-2 md:gap-3  md:m-[5rem] lg:columns-4 ">
